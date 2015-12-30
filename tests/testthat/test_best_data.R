1262a301f710f868c877863cbe800a2ed4f3d0a8
Carl Schroedl <cschroedl@usgs.gov>
2015-12-30 17:04:01 -0600

    Removing old files. Moving test util into main package so that it is easily accessible in the tests

--- tests/testthat/test_best_data.R
+++ tests/testthat/test_best_data.R
@@ -1,30 +1,10 @@
 library(testthat)
 library(validate)
-context("best data")
+context("annual flow")
 
 #looking for more thorough explanation of the 'validate' library capabilities?
 #Run:
 # vignette("intro", package="validate")
-
-expect_no_errors <- function(validation) {
-	the_summary <- validate::summary(validation)
-	error_count <- sum(the_summary$fails)
-	if(error_count){
-		msg <- paste(error_count, "errors detected", "\n")
-		msg <- paste(msg, paste(capture.output(the_summary), collapse = "\n"))
-		fail(msg)
-	} else {
-		expect_equal(TRUE, TRUE)
-	}
-	
-	barplot(validation, main = "check out dat data")
-}
-
-test_that("best data has a reasonable range of values", {
-	result <- validate::check_that(best_data, x > 0, x < 4, nchar(y) == 1)
-	expect_no_errors(result)
-	
-})
 
 test_that("annual flow's columns are correctly typed", {
 	result <- validate::check_that(aflow,
