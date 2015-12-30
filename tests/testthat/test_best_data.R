d91036f74eb28de32fd08eac2567e1e90ff0033e
Carl Schroedl <cschroedl@usgs.gov>
2015-12-30 16:10:18 -0600

    Added annual flow data and some basic checks.

--- tests/testthat/test_best_data.R
+++ tests/testthat/test_best_data.R
@@ -2,6 +2,10 @@ library(testthat)
 library(validate)
 context("best data")
 
+#looking for more thorough explanation of the 'validate' library capabilities?
+#Run:
+# vignette("intro", package="validate")
+
 expect_no_errors <- function(validation) {
 	the_summary <- validate::summary(validation)
 	error_count <- sum(the_summary$fails)
@@ -22,4 +26,15 @@ test_that("best data has a reasonable range of values", {
 	#print(str(errors(result)))
 	expect_no_errors(result)
 	
+})
+
+test_that("annual flow has a reasonable range of values", {
+	result <- validate::check_that(aflow, 
+																 FLOW > 0,
+																 FLOW < 1E10,
+																 nchar(SITE_ABB) == 4,
+																 WY < 2020,
+																 WY > 1950
+	)
+	expect_no_errors(result)
 })
