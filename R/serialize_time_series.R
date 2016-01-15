12e06e171b51a0845d683dd173b489176869496b
Carl Schroedl <cschroedl@usgs.gov>
2016-01-15 12:52:45 -0600

    whitespace

--- R/serialize_time_series.R
+++ R/serialize_time_series.R
@@ -1,11 +1,10 @@
 BASE_PATH <- 'inst/extdata/'
 EXTENSION <- '.csv'
-
 write_out <- function(dataframe_name){
-		dataframe <- get(dataframe_name)
-		file_name <- paste(BASE_PATH, dataframe_name, EXTENSION, sep = "")
-		write.csv(dataframe, file_name, quote = TRUE, na = "NULL")
-		return(file_name);
+	dataframe <- get(dataframe_name)
+	file_name <- paste(BASE_PATH, dataframe_name, EXTENSION, sep = "")
+	write.csv(dataframe, file_name, quote = TRUE, na = "NULL")
+	return(file_name);
 }
 
 serialize_time_series <- function(){
