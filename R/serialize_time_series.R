8189d385445d65b31fb525e8423affff0a811fbf
Carl Schroedl <cschroedl@usgs.gov>
2016-01-15 12:43:49 -0600

    Improving serialization.

--- R/serialize_time_series.R
+++ R/serialize_time_series.R
@@ -1,17 +1,21 @@
 BASE_PATH <- 'inst/extdata/'
 EXTENSION <- '.csv'
 
-write_out <- function(dataframe){
-		dataframe_name <- deparse(substitute(dataframe))
+write_out <- function(dataframe_name){
+		dataframe <- get(dataframe_name)
 		file_name <- paste(BASE_PATH, dataframe_name, EXTENSION, sep = "")
 		write.csv(dataframe, file_name, quote = TRUE, na = "NULL")
+		return(file_name);
 }
 
 serialize_time_series <- function(){
-	write_out(discqw)
-	write_out(aloads)
-	write_out(mloads)
-	write_out(aflow)
-	write_out(mflow)
-	write_out(dflow)
+	data_frame_names <- c(
+		'discqw',
+		'aloads',
+		'mloads',
+		'aflow',
+		'mflow',
+		'dflow'
+	)
+	lapply(data_frame_names, write_out)
 }
