7a298e0ceda361254e57764ecd04e34b27aef4c5
Carl Schroedl <cschroedl@usgs.gov>
2016-02-29 10:15:20 -0600

    Fixing wrong function call.

--- tests/testthat/test_annual_flow.R
+++ tests/testthat/test_annual_flow.R
@@ -44,7 +44,7 @@ test_that("annual flow has a reasonable range of values", {
 
 test_that("Flow data have the correct number of significant digits", {
   result <- validate::check_that(aflow, 
-                                 nchar(aflow$FLOW/1E11) <= 3
+                                 count_sig_figs(aflow$FLOW/1E11) <= 3
   )
   
   expect_no_errors(result)
