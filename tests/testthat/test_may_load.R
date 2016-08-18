3d888a424b10cc9ea88c08c50c7e6251e34b73de
Carl Schroedl <cschroedl@usgs.gov>
2016-08-18 09:28:45 -0500

    Formatting changes

--- tests/testthat/test_may_load.R
+++ tests/testthat/test_may_load.R
@@ -111,6 +111,8 @@ test_that("There are no duplicate values", {
    unique_columns <- mloads_without_ignored_modtypes[c('SITE_QW_ID', 'CONSTIT', 'WY')]
    duplicates <- unique_columns[duplicated(unique_columns),]
    if (nrow(duplicates) > 0) {
+       # prepare a helpful error message that starts with a descriptive summary and ends with
+   	   # R's handy native representation of the data frame containing duplicates.
        str_duplicates <- capture.output(duplicates)
        flat_str_duplicates <- paste(str_duplicates, "\n", sep = "", collapse = "\n")
        flat_str_duplicates
