548e2f167f8e9b5a09c00db2e490c39cbe8188e4
cjlee <cjlee@usgs.gov>
2017-03-01 09:16:03 -0600

    Fix name of pest_sites test

--- tests/testthat/test_pest_sites.R
+++ tests/testthat/test_pest_sites.R
@@ -50,7 +50,7 @@ test_that("All pestsites have a pesticide selected for aquatic-life and human-he
 })
 
 
-test_that("Pesticide percentage and names have ", {
+test_that("Pesticide percentage and names are not NA values", {
   pest_test1 <- pestsites[is.na(pestsites$PERC_HH)|is.na(pestsites$PERC_AQ)|is.na(pestsites$PERC_HH3)|is.na(pestsites$PERC_AQ3)|is.na(pestsites$PERC_HH_OLD)|is.na(pestsites$PERC_AQ_OLD)|is.na(pestsites$N_NEW)
                          |is.na(pestsites$NPEST_NEW_HH)|is.na(pestsites$N_3)|is.na(pestsites$NPEST_3_HH)|is.na(pestsites$N_OLD_AVE)|is.na(pestsites$N_YRS)|is.na(pestsites$N_OLD_HH)|is.na(pestsites$N_NEW_AQ)
                          |is.na(pestsites$NPEST_3_AQ)|is.na(pestsites$N_OLD_AQ)|is.na(pestsites$N_HH_OLD)|is.na(pestsites$N_AQ_OLD)|is.na(pestsites$PEST_NEW_EXCEED_HH)|is.na(pestsites$PEST_NEW_EXCEED_AQ)
